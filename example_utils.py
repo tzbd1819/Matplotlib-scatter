27e012b892defd3f660d7245206956edb9be9bf5
tzbd1819 <1819434425@qq.com>
2019-12-18 18:13:00 +0800

    Add files via upload

---
+++ example_utils.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# 创建画图fig和axes
+def setup_axes():
+    fig, axes = plt.subplots(ncols=3, figsize=(6.5,3))
+    for ax in fig.axes:
+        ax.set(xticks=[], yticks=[])
+    fig.subplots_adjust(wspace=0, left=0, right=0.93)
+    return fig, axes
+# 图片标题
+def title(fig, text, y=0.9):
+    fig.suptitle(text, size=14, y=y, weight='semibold', x=0.98, ha='right',
+                 bbox=dict(boxstyle='round', fc='floralwhite', ec='#8B7E66',
+                           lw=2))
+# 为数据添加文本注释
+def label(ax, text, y=0):
+    ax.annotate(text, xy=(0.5, 0.00), xycoords='axes fraction', ha='center',
+                style='italic',
+                bbox=dict(boxstyle='round', facecolor='floralwhite',
+                          ec='#8B7E66'))
